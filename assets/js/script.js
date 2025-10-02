de72f299d7f59f636cf76fdfa5d1234a05a2a8d6
Francisco Alisson Ribeiro da Costa <thelastofusalisson@gmail.com>
2025-10-02 06:34:39 -0300

    seguindo pra parte 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,15 @@
+//
+
+
+// Lista de botões
 const lista = document.getElementById("lista-filmes");
 const esquerda = document.getElementById("esquerda");
 const direita = document.getElementById("direita");
 
 direita.addEventListener("click", () => {
-    lista.scrollBy({left: 300, behavior: "smooth"});
+  lista.scrollBy({ left: 300, behavior: "smooth" });
 });
 
 esquerda.addEventListener("click", () => {
-    lista.scrollBy({ left: -300, behavior: "smooth"});
+  lista.scrollBy({ left: -300, behavior: "smooth" });
 });
